36ca2cd1da3cf4d6b106e0b38d12e74e0e0b13cf
EviePalaiochorinou <evangeliapal@gmail.com>
2021-12-07 15:49:59 +0000

    Fix a bug in Step 7 - connect to the database

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,10 +1,11 @@
 require 'bookmark'
 
-describe Bookmark do
-  let(:bookmark) { described_class }
+describe '.all' do
 
-  context '#all' 
-  it 'returns an array of bookmarks' do
-    expect(bookmark.all).to match_array(["http://www.makersacademy.com","http://www.destroyallsoftware.com","http://www.google.com"])
+  it 'returns a list of bookmarks' do
+    bookmarks = Bookmark.all
+    expect(bookmarks).to include "http://www.makersacademy.com"
+    expect(bookmarks).to include "http://www.google.com/"
+    expect(bookmarks).to include "http://www.destroyallsoftware.com"
   end 
 end
--- spec/features/list_of_bookmarks_spec.rb
+++ spec/features/list_of_bookmarks_spec.rb
@@ -1,13 +1,7 @@
-require 'capybara/rspec'
-require_relative '../../app'
-Capybara.app = BookmarkManager
-
 feature 'visiting Bookmarks' do
 
   scenario 'shows bookmarks' do
     visit('/bookmarks')
-    expect(page).to have_content "http://www.makersacademy.com"
-    expect(page).to have_content "http://www.destroyallsoftware.com"
-    expect(page).to have_content "http://www.google.com"
+    expect(page).to have_content ["http://www.makersacademy.com", "http://www.google.com/", "http://www.destroyallsoftware.com"]
   end
 end
